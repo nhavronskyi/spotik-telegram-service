159a920cbd290ff6942e26741ff753f54336def8
nazar <nhavronskyi@gmail.com>
2023-08-15 06:48:36 +0200

    new pr

---
+++ src/main/java/com/example/spotiktelegramservice/SpotikTelegramServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.spotiktelegramservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotikTelegramServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotikTelegramServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spotiktelegramservice/SpotikTelegramServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spotiktelegramservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotikTelegramServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
